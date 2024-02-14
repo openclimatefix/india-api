3e1da282c9bf9e512cdda97fbfc173591d892fa6
BumpVersion Action <bumpversion@github-actions>
2024-02-14 11:03:28 +0000

    Bump version: 0.1.7 → 0.1.8

--- src/india_api/internal/service/server.py
+++ src/india_api/internal/service/server.py
@@ -15,7 +15,7 @@ from india_api.internal import (
 from india_api.internal.models import ActualPower
 
 log = logging.getLogger(__name__)
-version = "0.1.7"
+version = "0.1.8"
 
 
 local_tz = pytz.timezone("Asia/Kolkata")
