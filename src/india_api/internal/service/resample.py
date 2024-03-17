b66745ee29fad1a5878318885cb7d53db3d4a44b
peterdudfield <peter.dudfield@hotmail.com>
2024-03-17 20:47:06 +0000

    round generation values

--- src/india_api/internal/service/resample.py
+++ src/india_api/internal/service/resample.py
@@ -17,6 +17,9 @@ def resample_generation(values: list[ActualPower], internal_minutes: int) -> lis
     # resample
     df = df.set_index("Time").resample(f"{internal_minutes}T").mean().dropna()
 
+    # convert to PowerKW to integers
+    df.PowerKW = df.PowerKW.astype(int)
+
     # convert back to list of ActualPower
     return [
         ActualPower(
