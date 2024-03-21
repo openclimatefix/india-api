9be819ea6a4dcfd33115b8fc07d3742ff8778397
BumpVersion Action <bumpversion@github-actions>
2024-03-21 10:43:39 +0000

    Bump version: 0.1.12 → 0.1.13

--- src/india_api/internal/service/server.py
+++ src/india_api/internal/service/server.py
@@ -18,7 +18,7 @@ from india_api.internal.models import ActualPower
 from india_api.internal.service.resample import resample_generation
 
 log = logging.getLogger(__name__)
-version = "0.1.12"
+version = "0.1.13"
 
 
 local_tz = pytz.timezone("Asia/Kolkata")
