f7d6c497b9a3ebe1d8522afd008391664d4861d7
peterdudfield <peter.dudfield@hotmail.com>
2024-09-27 15:29:28 +0100

    fix version import

--- src/india_api/internal/service/__init__.py
+++ src/india_api/internal/service/__init__.py
@@ -1,4 +1,4 @@
-from .server import server
+from .server import server, version
 from .database_client import get_db_client
 
-__all__ = ["server", "get_db_client"]
+__all__ = ["server", "get_db_client", "version"]
