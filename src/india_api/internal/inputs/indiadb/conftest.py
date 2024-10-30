1e2f00e909e19756a555e99a9281da025db9c092
Rupam golui <142140947+Itz-Agasta@users.noreply.github.com>
2024-10-30 18:40:10 +0530

    Change 'ruvnl' Label to 'rajasthan'

--- src/india_api/internal/inputs/indiadb/conftest.py
+++ src/india_api/internal/inputs/indiadb/conftest.py
@@ -66,7 +66,7 @@ def sites(db_session):
         asset_type="pv",
         country="india",
         region='testID',
-        client_site_name='ruvnl_pv_testID1'
+        client_site_name='rajasthan_pv_testID1'
     )
     db_session.add(site)
     sites.append(site)
@@ -81,7 +81,7 @@ def sites(db_session):
         asset_type="wind",
         country="india",
         region='testID',
-        client_site_name = 'ruvnl_wind_testID'
+        client_site_name = 'rajasthan_wind_testID'
     )
     db_session.add(site)
     sites.append(site)
