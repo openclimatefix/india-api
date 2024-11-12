b86f9b8691702a91bd558cd2a94963d274a995d1
BumpVersion Action <bumpversion@github-actions>
2024-11-12 10:48:23 +0000

    Bump version: 0.1.44 → 0.1.45

--- src/india_api/internal/service/server.py
+++ src/india_api/internal/service/server.py
@@ -14,7 +14,7 @@ from india_api.internal.service.sites import router as sites_router
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 log = logging.getLogger(__name__)
-version = "0.1.44"
+version = "0.1.45"
 
 
 tags_metadata = [
